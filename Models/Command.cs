47b09126392440d826d214dea50119b0c794a36f
George Ghibirdic <george.ghibirdic@gmail.com>
2021-02-21 19:11:55 +0200

    Added Command model

---
+++ Models/Command.cs
@@ -0,0 +1,10 @@
+namespace CommandKeeper.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public int MyProperty { get; set; }
+        public string Platform { get; set; }
+    }
+}
